dac17abe4a21eff266d75972a3c55060c07f62cc
Hedi Ben Fraj <hedibenfraj98@gmail.com>
2025-06-23 16:45:09 +0000

    in the dashboards sidebar, i want to be able to see the list of items me

--- src/app/admin/projects/[projectId]/layout.tsx
+++ src/app/admin/projects/[projectId]/layout.tsx
@@ -1,5 +1,6 @@
 import { ViewsProvider } from '@/contexts/views-context';
 import Breadcrumbs from '@/components/admin/breadcrumbs';
+import ProjectSidebar from '@/components/admin/project-sidebar';
 
 export default function ProjectDetailLayout({
   children,
@@ -10,12 +11,17 @@ export default function ProjectDetailLayout({
 }) {
   return (
     <ViewsProvider projectId={projectId}>
-      <header className="h-16 flex-shrink-0 flex items-center px-6 border-b border-neutral-700 bg-[#2a2a2a]">
-        <Breadcrumbs />
-      </header>
-      <main className="flex-1 p-8 bg-[#313131] overflow-y-auto">
-        {children}
-      </main>
+      <div className="flex flex-1">
+        <ProjectSidebar projectId={projectId} />
+        <div className="flex-1 flex flex-col">
+          <header className="h-16 flex-shrink-0 flex items-center px-6 border-b border-neutral-700 bg-[#2a2a2a]">
+            <Breadcrumbs />
+          </header>
+          <main className="flex-1 p-8 bg-[#313131] overflow-y-auto">
+            {children}
+          </main>
+        </div>
+      </div>
     </ViewsProvider>
   );
 }
